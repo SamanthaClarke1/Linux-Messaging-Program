6e63f59f8fdb6779aa3851ed166f154931146d0f
Samuel Clarke <clarkepearl44@gmail.com>
2018-05-01 11:09:55 +1000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+yum install -y python34
+yum install -y python34-devel
+yum install -y python34-pip
+sudo easy_install-3.4 pip
+pip3 install --upgrade pip
+pip3 install --upgrade pycrypto
+cp -r /Volumes/RS01/Resources/Engineering/Sam/Linux-Messaging-Program-master/ /home/cmls/.SAMchat/
+
